c6421a5ee58772036b5adb951241f99cd0f1e1cd
saras shrestha <sarasstha5@gmail.com>
2026-07-01 07:07:54 +0545

    default value description added

--- 05-Query-parameter.py
+++ 05-Query-parameter.py
@@ -8,10 +8,10 @@ def get_user(name: str):
         "user": name
     }
 
-@app.get("/users")
-def get_user(name: str="Guest"):
-    return{
-        "user": name
-    }
-       
-         
+#default value: You can set a default value for a query parameter by assigning it in the function definition. If the client does not provide a value, the default will be used.
+# @app.get("/users")
+# def get_user(name: str="Guest"):
+#     return{
+#         "user": name
+#     }
+
