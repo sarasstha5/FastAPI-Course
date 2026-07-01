66f79bbce30573ddc1a25876a2b6c8078f413fcd
saras shrestha <sarasstha5@gmail.com>
2026-07-02 00:57:10 +0545

    using pydantic baseModel

--- 06-post-handling.py
+++ 06-post-handling.py
@@ -1,15 +1,29 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 app = FastAPI()
 
 
-@app.post("/users")
+#@app.post("/users")
 # def create_user(name: str , age: int):
 #     return{
 #         "user": name,
 #         "age": age
 #     }
 
-def create_user(user: dict):
+#using dict: You can also use a dictionary to send data to the server. However, this approach does not provide validation or serialization of the data.
+# @app.post("/users")
+# def create_user(user: dict):
+#     return{
+#         "user": user
+#     }
+
+#pydantic model: Pydantic models are used to define the structure of the data that is expected in the request body. They provide validation and serialization of the data.
+class User(BaseModel):
+    name: str
+    age: int
+    
+@app.post("/users")
+def create_user(user: User):
     return{
         "user": user
     }
