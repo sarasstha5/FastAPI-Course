6aae47f2c83c1915f8385ed8db95d942f9973b52
saras shrestha <sarasstha5@gmail.com>
2026-07-01 07:28:04 +0545

    multiiple query parameter

--- 05-Query-parameter.py
+++ 05-Query-parameter.py
@@ -1,17 +1,31 @@
 from fastapi import FastAPI
 app = FastAPI()
 
-#query parameter: A query parameter is a piece of data sent in the URL after a '?'
-@app.get("/users")
-def get_user(name: str):
-    return{
-        "user": name
-    }
+# #query parameter: A query parameter is a piece of data sent in the URL after a '?'
+# @app.get("/users")
+# def get_user(name: str):
+#     return{
+#         "user": name
+#     }
 
-#default value: You can set a default value for a query parameter by assigning it in the function definition. If the client does not provide a value, the default will be used.
+# # default value: You can set a default value for a query parameter by assigning it in the function definition. If the client does not provide a value, the default will be used.
 # @app.get("/users")
 # def get_user(name: str="Guest"):
 #     return{
 #         "user": name
 #     }
 
+# #optional parameter
+# @app.get("/users")
+# def get_user(name: str=None):
+#     return{
+#         "user": name
+#     }
+
+#multiple query parameters: You can define multiple query parameters by adding more parameters to the function definition.
+@app.get("/users")
+def get_user(name: str=None, age: int=None):
+    return{
+        "user": name,
+        "age": age
+    }
