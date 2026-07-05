7ef17b5e57b049379151992fc811a41c59c37e39
saras shrestha <sarasstha5@gmail.com>
2026-07-05 07:45:52 +0545

    todo-put API used

--- 08-CRUD.py
+++ 08-CRUD.py
@@ -10,6 +10,7 @@ class Todo(BaseModel):
     Title:str
     completion:bool
 
+#create todo
 @app.post("/todos")
 def creat_todo(todo:Todo):
     Todos_list.append(todo)
@@ -18,18 +19,33 @@ def creat_todo(todo:Todo):
         "data": todo
     }
 
+#get all list of todos
 @app.get("/todos")
 def get_todos():
     return{
         "data": Todos_list
     }
 
+#get sepecific todo using query parameter
 @app.get("/todos/{todo_id}")
 def get_todo(todo_id:int):
     for todo in Todos_list:
         if todo.Id == todo_id:
             return{
                 "data":todo
+            }
+    return{
+        "message":"todo not found"
+    }
+
+@app.put("/todos/{todo_id}")
+def update_todo(todo_id:int,updated_todo:Todo):
+    for index,t in enumerate(Todos_list):
+        if t.Id == todo_id:
+            Todos_list[index] = updated_todo
+            return{
+                "message":"todo updated",
+                "data": updated_todo
             }
     return{
         "message":"todo not found"
