ac724de8dc5380b40dd4f1ffa483c42e9d0b5a00
saras shrestha <sarasstha5@gmail.com>
2026-08-17 18:15:05 +0545

    basic-logging request through middleware

--- 14-middleware.py
+++ 14-middleware.py
@@ -4,15 +4,15 @@ app = FastAPI()
 #middleware is a function that runs when client send request and request first goes to middle ware which is like checkpoint not actual endpoint which helps in verifying the request and goes to the actual endpoint function.
 #It can be used to perform tasks such as logging, authentication, and modifying the request or response.
 
-@app.middleware("http")
-async def middleWare(request:Request,call_next):
-    print("1. Request reached middleware from client")
+# @app.middleware("http")
+# async def middleWare(request:Request,call_next):
+#     print("1. Request reached middleware from client")
 
-    response = await call_next(request)  #this will call the next middleware or the actual endpoint function. It will return a response object.
+#     response = await call_next(request)  #this will call the next middleware or the actual endpoint function. It will return a response object.
 
-    print("4. Response came back to middleware")
+#     print("4. Response came back to middleware")
 
-    return response
+#     return response
 
 @app.get("/user")
 def get_user():
@@ -20,4 +20,13 @@ def get_user():
     return{
         "message":"Hello user"
     }
+
+#Logging requests
+@app.middleware("http")
+async def log_request(request: Request, call_next):
+    print(request.method, request.url)
+
+    response = await call_next(request)
+
+    return response
 
