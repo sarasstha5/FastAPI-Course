9115f8584e715619ea4f5aa901fd27568b0fb7f7
saras shrestha <sarasstha5@gmail.com>
2026-09-02 00:43:24 +0545

    crud operation with ORM- create and read

---
+++ 17-crud.py
@@ -0,0 +1,63 @@
+from fastapi import FastAPI, Depends, HTTPException, status
+from sqlalchemy import Boolean, create_engine, Column, Integer, String
+from sqlalchemy.orm import Session, sessionmaker, declarative_base
+
+app = FastAPI()
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+#create table in database named "todos"
+class Todo(Base):
+    __tablename__ = "todos"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    description = Column(String, index=True)
+    completed = Column(Boolean, default=False)
+
+Base.metadata.create_all(bind = engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+#create API
+@app.post("/todos")
+def create_todo(title:str, description:str, db:Session = Depends(get_db)):
+    todo = Todo(
+        title=title,
+        description=description,
+        completed=False
+    )
+    db.add(todo)
+    db.commit()
+    db.refresh(todo)
+    return {
+        "message": "Todo created successfully",
+        "todo": todo
+    }
+#read all data from database
+@app.get("/todos")
+def get_todos(db:Session = Depends(get_db)):
+    todos = db.query(Todo).all()
+    return todos
+
+#read single data from database
+@app.get("/todos/{todo_id}")
+def get_todo(todo_id:int, db:Session = Depends(get_db)):
+    todo = db.query(Todo).filter(Todo.id == todo_id).first() #filter() method is used to filter the results based on a condition. 
+    if not todo:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Todo not found"
+        )
+    return {
+        "message": "Todo retrieved successfully",
+        "todo": todo
+    }
